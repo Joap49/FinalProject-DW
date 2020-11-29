51fabfc28e39be141424203b2fe99839e560b3a7
Joanna Pilika <joannap@Joannas-MacBook-Pro.local>
2020-11-29 17:50:58 -0500

    Deleted code in app.js

--- finalproject-backend/app.js
+++ finalproject-backend/app.js
@@ -1,34 +1,33 @@
 // Backend Application for Final Project
 //import React, { useEffect, useState } from "react";
 //import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
-import firebase from "firebase/app"; //import everything from this package as firebase
-import "firebase/auth"; //importing entire package
+// import firebase from "firebase/app"; //import everything from this package as firebase
+// import "firebase/auth"; //importing entire package
 //Styles
 import "./App.css";
 
 //Web app's firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY, // replace with .env file API reference
-  authDomain: "final-project-dw2020.firebaseapp.com",
-  databaseURL: "https://final-project-dw2020.firebaseio.com",
-  projectId: "final-project-dw2020",
-  storageBucket: "final-project-dw2020.appspot.com",
-  messagingSenderId: "978674596080",
-  appId: "1:978674596080:web:e671f444cc0e36c66cea35",
-};
-const firebase = require("firebase");
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_APIKEY, // replace with .env file API reference
+//   authDomain: "final-project-dw2020.firebaseapp.com",
+//   databaseURL: "https://final-project-dw2020.firebaseio.com",
+//   projectId: "final-project-dw2020",
+//   storageBucket: "final-project-dw2020.appspot.com",
+//   messagingSenderId: "978674596080",
+//   appId: "1:978674596080:web:e671f444cc0e36c66cea35",
+// };
+// const firebase = require("firebase");
 
 function App() {
-  const [loggedIn, setloggedIn] = useState(false); //determine if logged in
-  const [loading, setLoading] = useState(true); //is page loading
-
-  //Ensure app is initialized when its ready
-  useEffect(() => {
-    //initializes Firebase
-    if (!firebase.apps.legnth) {
-      firebase.initializeApp(firebaseConfig);
-    }
-  }, [firebaseConfig]);
+  //   const [loggedIn, setloggedIn] = useState(false); //determine if logged in
+  //   const [loading, setLoading] = useState(true); //is page loading
+  //   //Ensure app is initialized when its ready
+  //   useEffect(() => {
+  //     //initializes Firebase
+  //     if (!firebase.apps.legnth) {
+  //       firebase.initializeApp(firebaseConfig);
+  //     }
+  //   }, [firebaseConfig]);
 }
 
 export default App;
