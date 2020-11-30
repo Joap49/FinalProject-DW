f19de6375e96a848c8c59529951c14c7b0984329
Joanna Pilika <joannap@Joannas-MacBook-Pro.local>
2020-11-29 23:44:31 -0500

    updated app.js

--- finalproject-backend/app.js
+++ finalproject-backend/app.js
@@ -1,32 +1,35 @@
-// Backend Application for Final Project
-//import React, { useEffect, useState } from "react";
-//import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
-// import firebase from "firebase/app"; //import everything from this package as firebase
-// import "firebase/auth"; //importing entire package
+//Backend Application for Final Project
+//import firebase from "firebase/app"; //import everything from this package as firebase
+import "firebase/app/dist/index.cjs.js";
+import "@firebase/auth";
+import "@firebase/firestore";
 //Styles
 
 //Web app's firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_APIKEY, // replace with .env file API reference
-//   authDomain: "final-project-dw2020.firebaseapp.com",
-//   databaseURL: "https://final-project-dw2020.firebaseio.com",
-//   projectId: "final-project-dw2020",
-//   storageBucket: "final-project-dw2020.appspot.com",
-//   messagingSenderId: "978674596080",
-//   appId: "1:978674596080:web:e671f444cc0e36c66cea35",
-// };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY, // replace with .env file API reference
+  authDomain: "final-project-dw2020.firebaseapp.com",
+  databaseURL: "https://final-project-dw2020.firebaseio.com",
+  projectId: "final-project-dw2020",
+  storageBucket: "final-project-dw2020.appspot.com",
+  messagingSenderId: "978674596080",
+  appId: "1:978674596080:web:e671f444cc0e36c66cea35",
+};
 // const firebase = require("firebase");
 
+// const express = require("express");
+// const app = express();
+
 function App() {
-  //   const [loggedIn, setloggedIn] = useState(false); //determine if logged in
-  //   const [loading, setLoading] = useState(true); //is page loading
-  //   //Ensure app is initialized when its ready
-  //   useEffect(() => {
-  //     //initializes Firebase
-  //     if (!firebase.apps.legnth) {
-  //       firebase.initializeApp(firebaseConfig);
-  //     }
-  //   }, [firebaseConfig]);
+  const [loggedIn, setloggedIn] = useState(false); //determine if logged in
+  const [loading, setLoading] = useState(true); //is page loading
+  //Ensure app is initialized when its ready
+  useEffect(() => {
+    //initializes Firebase
+    if (!firebase.apps.legnth) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }, [firebaseConfig]);
 }
 
 export default App;
